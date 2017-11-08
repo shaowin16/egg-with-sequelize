1c05e5b04aa7c4561fe6a78f6c07e45cec7ee486
shaowin16 <shaowinSole@163.com>
2017-11-08 21:43:07 +0800

    fix

--- config/config.local.js
+++ config/config.local.js
@@ -24,26 +24,6 @@ module.exports = () => {
       consoleLevel: 'DEBUG',
     },
 
-    nats: {
-      subject: 'indexy.articles',
-      clusterId: 'test-cluster',
-      clientId: (() => {
-        return 'node-stream-pub_' + (process.env.nats_cid || '');
-      })(),
-      opts: {
-        url: 'nats://114.67.159.3:4222',
-        maxReconnectAttempts: -1, // 一直重试
-        reconnectTimeWait: 1000, // 1s
-      },
-      // 订阅时可配置的参数
-      subscriptionOptions: {
-        durableName: 'crawler-workers',
-        deliverAllAvailable: true,
-        maxInFlight: 5, // 一次只接收处理5个消息
-        // ackWait: 60 * 1000,
-        manualAckMode: true, // 手动ack
-      },
-    },
 
   };
 
